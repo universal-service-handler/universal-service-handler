6da706047cde927d8feee13b790e824ccf13c88a
Zsolt Javorszky <zsolt.javorszky@gmail.com>
2013-04-01 10:38:54 +0200

    Added License.

--- src/UniversalServiceHandler/Service.php
+++ src/UniversalServiceHandler/Service.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * @author Zsolt Javorszky
+ * @author Sandor Legoza
+ * @copyright 2013
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace UniversalServiceHandler;
 
 use Optionable;
